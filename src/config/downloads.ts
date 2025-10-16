75bf10c56f3839d4767a2016727420015675e263
ykggtrembala123 <140025450+ykggtrembala123@users.noreply.github.com>
2025-10-16 01:22:25 -0300

    Update downloads.ts

--- src/config/downloads.ts
+++ src/config/downloads.ts
@@ -1,8 +1,8 @@
 // Centralized download configuration
 export const downloadLinks = {
-  windows: "https://cdn.discordapp.com/attachments/1428134547219943465/1428224340977123409/EchoPlayer.rar?ex=68f1b912&is=68f06792&hm=87e5327e0801c31568a659ac152233c00d7505bc7e53ef2e5a1ba099c63aa5a8&",
-  mac: "https://cdn.discordapp.com/attachments/1428134547219943465/1428224340977123409/EchoPlayer.rar?ex=68f1b912&is=68f06792&hm=87e5327e0801c31568a659ac152233c00d7505bc7e53ef2e5a1ba099c63aa5a8&",
-  linux: "https://cdn.discordapp.com/attachments/1428134547219943465/1428224340977123409/EchoPlayer.rar?ex=68f1b912&is=68f06792&hm=87e5327e0801c31568a659ac152233c00d7505bc7e53ef2e5a1ba099c63aa5a8&",
+  windows: "https://www.dropbox.com/scl/fi/79l91dbwchrrgmunqu2ge/EchoPlayer.rar?dl=1",
+  mac: "https://www.dropbox.com/scl/fi/79l91dbwchrrgmunqu2ge/EchoPlayer.rar?dl=1",
+  linux: "https://www.dropbox.com/scl/fi/79l91dbwchrrgmunqu2ge/EchoPlayer.rar?dl=1",
 } as const;
 
 export type Platform = keyof typeof downloadLinks;
